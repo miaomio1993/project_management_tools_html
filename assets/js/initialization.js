3a81826794659e068802493dcfefebb6aef0014f
郭阿强 <786492804@qq.com>
2018-11-17 13:22:57 +0800

    1

--- assets/js/initialization.js
+++ assets/js/initialization.js
@@ -1,5 +1,6 @@
 // 全局url
-var url = "http://192.168.43.51/smmp/";
+var url = "http://localhost:8089/smmp/";
+
 // 全局ajax请求配置
 $.ajaxSetup({
 	dataType: "json",
@@ -10,12 +11,24 @@ $.ajaxSetup({
 
 /**
  * 获取url里面的参数(name)
- *
  **/
 function getUrlParam(name) {
-   var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
-   var r = encodeURI(window.location.search).substr(1).match(reg);  //匹配目标参数
-   if (r != null)
-	   return decodeURI(unescape(r[2]));
-   return null; //返回参数值
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+  var r = encodeURI(window.location.search).substr(1).match(reg);  //匹配目标参数
+  if (r != null)
+	  return decodeURI(unescape(r[2]));
+  return null; //返回参数值
+}
+
+
+
+/**
+ * 格式化时间戳
+ */
+function formatDate(time){
+	var date = new Date(time);
+	var ymd = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
+	var hms = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+	var ymdhms = ymd + " " + hms;
+	return ymdhms;
 }
